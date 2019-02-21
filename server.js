230eb8a8879f0600db00e1a5178a0c0cb5c9a818
Steve Shannon <you@example.com>
2019-02-21 18:36:34 -0500

    THANK YOU STACY

--- server.js
+++ server.js
@@ -49,10 +49,80 @@ app.get("/", function(req, res) {
         }
         else {
           // Otherwise, send the Posts we found to the browser as a json
-          res.render("index", found);
+          res.render("index", {posts:found});
         }
+        console.log(found)
       });
 })
+
+
+app.get("/scrape", function(req, res) {
+
+  // Parses our HTML and helps us find elements
+var cheerio = require("cheerio");
+// Makes HTTP request for HTML page
+var axios = require("axios");
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from reddit's webdev board:" +
+            "\n***********************************\n");
+
+// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("p.title").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = $(element).text();
+
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).children().attr("href");
+
+    var summary = $(element).siblings(".tagline").text()
+
+    var image = $(element).parent().parent().siblings(".thumbnail").children("img").attr("src")
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    // object we're pushing into mongo
+    results.push({
+      title: title,
+
+      link: link,
+
+      summary: summary,
+
+      image:image
+    });
+
+    // save result object in a db
+    db.posts.insert({
+      title: title,
+      link: link,
+      summary: summary,
+      image:image
+     
+    })
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+  
+
+});
+})
+
 
 // Post a Post to the mongoose database
 app.post("/submit", function(req, res) {
@@ -178,45 +248,3 @@ app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
 
-// Parses our HTML and helps us find elements
-var cheerio = require("cheerio");
-// Makes HTTP request for HTML page
-var axios = require("axios");
-
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from reddit's webdev board:" +
-            "\n***********************************\n");
-
-// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
-axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  $("p.title").each(function(i, element) {
-
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
